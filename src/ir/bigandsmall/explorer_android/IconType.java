d8b39e1f5ee87cd8ef9f01e977167056083c52c5
hasanesfahan <hasan.sepronik@gmail.com>
2014-11-01 14:22:13 -0700

    add icon word excel power-point text pdf

--- src/ir/bigandsmall/explorer_android/IconType.java
+++ src/ir/bigandsmall/explorer_android/IconType.java
@@ -11,18 +11,30 @@ public class IconType {
 	
 	public int getIconId()
 	{
-		if(getExtention().equalsIgnoreCase(".asf"))
-			return R.drawable.ic_secure;
+
+		if(getExtention().equalsIgnoreCase("pdf"))
+			return R.drawable.ic_pdf;
+		
+		else if((getExtention().equalsIgnoreCase("doc")||getExtention().equalsIgnoreCase("docx")))
+			return R.drawable.ic_word;
+		
+		else if((getExtention().equalsIgnoreCase("ppt"))||(getExtention().equalsIgnoreCase("pptx")))
+			return R.drawable.ic_powerpoint;
+		
+		else if((getExtention().equalsIgnoreCase("xls"))||(getExtention().equalsIgnoreCase("xlsx")))
+			return R.drawable.ic_excel;
+
+		else if(getExtention().equalsIgnoreCase("txt"))
+			return R.drawable.ic_txt;
+ 
 		else 
 			return R.drawable.ic_file;
-				
 	}
-	
-	
+
 	public String getExtention()
 	{
-		if(FileName.lastIndexOf(".")>0)
-			return FileName.substring(FileName.lastIndexOf("."));
+		if((FileName.lastIndexOf(".")-1)>0)
+			return FileName.substring(FileName.lastIndexOf(".")+1);
 
 		return "";
 	}
