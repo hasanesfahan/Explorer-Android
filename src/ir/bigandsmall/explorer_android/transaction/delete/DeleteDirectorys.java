8079e783bf99772d63f71341a04f60d512475c48
hasanesfahan <hasan.sepronik@gmail.com>
2014-11-11 17:34:59 -0800

    add package delete and move files (delete) from other package to this package.

--- src/ir/bigandsmall/explorer_android/transaction/delete/DeleteDirectorys.java
+++ src/ir/bigandsmall/explorer_android/transaction/delete/DeleteDirectorys.java
@@ -1,4 +1,4 @@
-package ir.bigandsmall.explorer_android.transaction;
+package ir.bigandsmall.explorer_android.transaction.delete;
 
 import java.io.File;
 
--- src/ir/bigandsmall/explorer_android/transaction/delete/DialogDelete.java
+++ src/ir/bigandsmall/explorer_android/transaction/delete/DialogDelete.java
@@ -1,4 +1,4 @@
-package ir.bigandsmall.explorer_android.transaction;
+package ir.bigandsmall.explorer_android.transaction.delete;
 
 import ir.bigandsmall.explorer_android.FileSpecifications;
 import ir.bigandsmall.explorer_android.MainActivity;
